dac2f677116f053d3678789564a4ddb24049d4a3
Dezzelshipc <86155929+Dezzelshipc@users.noreply.github.com>
2022-10-09 22:16:34 +1000

    Task2: UserService
    - Обёртка в Result.
    - Интерфейс репозитория.
    - UseCases для User.
    - Unit тесты.

---
+++ domain/Models/User.cs
@@ -0,0 +1,44 @@
+﻿using domain.Logic;
+
+namespace domain.Models
+{
+    public class User
+    {
+        public int Id;
+        public string PhoneNumber;
+        public string Fio;
+        public Role Role = Role.Patient;
+
+        public string UserName;
+        public string Password;
+
+        public User() { }
+        public User(int id, string phoneNumber, string fio, Role role, string userName, string password)
+        {
+            Id = id;
+            PhoneNumber = phoneNumber;
+            Fio = fio;
+            Role = role;
+            UserName = userName;
+            Password = password;
+        }
+
+        public Result IsValid()
+        {
+            if (string.IsNullOrEmpty(UserName))
+                return Result.Fail("Username error");
+
+            if (string.IsNullOrEmpty(Password))
+                return Result.Fail("Password error");
+
+            if (string.IsNullOrEmpty(PhoneNumber))
+                return Result.Fail("Phone number error");
+
+            if (string.IsNullOrEmpty(Fio))
+                return Result.Fail("Fio error");
+
+            return Result.Ok();
+        }
+    }
+
+}
---
+++ domain/UseCases/UserService.cs
@@ -0,0 +1,43 @@
+﻿using domain.Logic;
+using domain.Models;
+
+namespace domain.UseCases
+{
+    public class UserService {
+        private IUserRepository _db;
+
+        public UserService(IUserRepository db)
+        { 
+            _db = db;
+        }
+
+        public Result<User> Register(User user) 
+        {
+            var check = user.IsValid();
+            if (check.IsFailure)
+                return Result.Fail<User>(check.Error);
+
+            if (_db.IsUserExists(user.UserName))
+                return Result.Fail<User>("Username already exists");
+
+
+            return _db.CreateUser(user) ? Result.Ok(user) : Result.Fail<User>("User creating error");
+        }
+
+        public Result<User> GetUserByLogin(string login)
+        {
+            if (string.IsNullOrEmpty(login))
+                return Result.Fail<User>("Login error");
+
+            return _db.IsUserExists(login) ? Result.Ok(_db.GetUserByLogin(login)) : Result.Fail<User>("User not found");
+        }
+
+        public Result IsUserExists(string login)
+        {
+            if (string.IsNullOrEmpty(login))
+                return Result.Fail("Login error");
+
+            return _db.IsUserExists(login) ? Result.Ok() : Result.Fail("User not found");
+        }
+    }
+}
